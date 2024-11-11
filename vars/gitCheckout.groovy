dfd0bf194987691b866ecadd188d167bf831fb10
Opqtech <optrainings@opqbootcamp.com>
2024-11-11 22:35:42 +0530

    added groovy scripts

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,5 @@
+
+def call (String gitRepo, String gitBranch, String gitCredID) {
+  checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredID", url: "$gitRepo"]]])
+}
+
