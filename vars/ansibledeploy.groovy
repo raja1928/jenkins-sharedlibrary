abf9adb6edb5417a1d3058b9652cce46017106dc
raja1928 <53326027+raja1928@users.noreply.github.com>
2024-12-23 15:14:09 +0530

    Update ansibledeploy.groovy

--- vars/ansibledeploy.groovy
+++ vars/ansibledeploy.groovy
@@ -1,1 +1,14 @@
-d
+def call(String dockerRegistry, String dockerImageTag, String helmChartName, String awsCredID, String awsRegion, String eksClusterName, String kubernetesNamespace = 'default') {
+
+    // Determine which values file to use based on the namespace
+    if (kubernetesNamespace == "dev") {
+        valuesFile = "DEV.yaml"
+    } else if (kubernetesNamespace == "uat") {
+        valuesFile = "UAT.yaml"
+    }
+
+    // Deploy using Helm
+    sh "helm upgrade --install ${helmChartName} helm/ --namespace ${kubernetesNamespace} --create-namespace --set image.repository=${dockerRegistry} --set image.tag=${dockerImageTag} -f helm/values/${valuesFile}"
+}
+
+
